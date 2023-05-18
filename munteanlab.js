be73bb583cb90e5b159f64555941bbf418ddb64d
jowidjaja <116462552+jowidjaja@users.noreply.github.com>
2023-05-17 21:04:40 -0400

    Add files via upload

---
+++ munteanlab.js
@@ -0,0 +1,46 @@
+const activePage = window.location.pathname;
+console.log(activePage);
+
+const navLinks = document.querySelectorAll ('nav a').
+forEach(link => {
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add('active');
+    }
+})
+
+
+
+Fancybox.bind("[data-fancybox]", {
+// Your custom options
+});
+
+
+const buttons = document.querySelectorAll('button');
+const popUp = document.querySelector('.pop-up');
+
+const people = [
+    {
+        name: "Brian Muntean",
+        about: "Lorem Ipsum",
+    },
+    {
+        name: "Douglas Sloan",
+        about: "Lorem ipsum",
+    },
+]
+
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        console.log(event.target.id);
+        popUp.innerHTML = 
+        `<h3>${people[event.target.id].name}</h3>
+        <p>${people[event.target.id].about}</p>
+        <img src="${people[event.target.id].imageSrc}">
+        <button onClick="togglePopUp()">Close Window</button>`;
+        popUp.classList.toggle('visible');
+    })
+});
+
+function togglePopUp() {
+    popUp.classList.toggle('visible');
+}
